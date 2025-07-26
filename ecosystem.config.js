d5814420b8172a7f4cd78cca7e19b6ef3ace6ac6
Andrey Sokolov <a.sokolov@iblschool.ru>
2025-07-26 18:40:10 +0300

    +

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '.env.deploy' });
+require('dotenv').config();
 
 const {
   DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/main', REPO_URL, DEPLOY_PORT
