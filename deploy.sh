9ae37a8dd6639a7cd668ff6c83413e96f2e42bb6
Andrey Sokolov <a.sokolov@iblschool.ru>
2025-07-26 16:30:20 +0300

    +

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+echo "Backend:"
+cd "${DEPLOY_PATH}/current/backend"
+yarn install
+yarn build
+
+echo "Frontend:"
+cd "${DEPLOY_PATH}/current/frontend"
+yarn install --production
+yarn build
+
+echo "Pm2:"
+npm install dotenv
+pm2 startOrRestart "${DEPLOY_PATH}/current/ecosystem.config.js" --env production
+pm2 save
+
+echo "Completed"
