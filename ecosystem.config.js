94c3bf724527ff892aee70f61e3bfbf0729b8c6b
Andrey Sokolov <a.sokolov@iblschool.ru>
2025-07-26 19:32:19 +0300

    +

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: '.env.deploy' });
 
 const {
   DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/main', REPO_URL, DEPLOY_PORT
@@ -7,30 +7,40 @@ const {
 module.exports = {
   apps: [
     {
-      name: "back",
-      script: "dist/app.js",
-      cwd: `./backend`,
-      autorestart: true
+      name: 'backend',
+      script: './backend/app.js',
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: DEPLOY_PORT
+      },
     },
     {
-      name: "front",
-      script: "serve",
-      env: {
-        PM2_SERVE_PATH: "frontend/build",
-        PM2_SERVE_PORT: 3001
+      name: 'frontend',
+      script: 'serve',
+      args: ['-s', 'frontend/build', '-l', '80'],
+      env_production: {
+        PM2_SERVE_PATH: 'frontend/build',
+        PM2_SERVE_PORT: 80,
+        NODE_ENV: 'production'
       }
-    },
+    }
   ],
   deploy: {
     production: {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
-      port: DEPLOY_PORT || 22,
       ref: DEPLOY_REF,
       repo: REPO_URL,
-      path: DEPLOY_PATH,
-      'pre-deploy-local': `scp -P ${DEPLOY_PORT} ./backend/.env.production ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/backend/.env`,
-      'post-deploy': `cd ${DEPLOY_PATH}/current && DEPLOY_PATH=${DEPLOY_PATH} ./deploy.sh`
-    },
-  },
+      path: `/home/${DEPLOY_USER}/mesto`,
+      'pre-deploy-local': `
+        echo 'Deploying to production...';
+        scp -r ./backend/.env.production ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend/.env;
+      `,
+      'post-deploy': `
+        cd backend && npm install && pm2 startOrRestart ecosystem.config.js --env production;
+        cd ../frontend && npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production;
+        pm2 save;
+      `
+    }
+  }
 };
